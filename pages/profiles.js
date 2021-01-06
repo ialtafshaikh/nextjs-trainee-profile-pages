939330ae6336950473094c582aa3565b1236e68a
ialtafshaikh <altaf.shaikh@contentstack.com>
2021-01-06 21:57:48 +0530

    implemented profile as pre-rendering

--- pages/profiles.js
+++ pages/profiles.js
@@ -1,10 +1,22 @@
-import CardList from "../components/CardList";
+import axios from "axios";
 
-export default function Profiles() {
+import CardList from "../components/CardList";
+import { employees } from "../constants/endpoints";
+
+export default function Profiles(props) {
   return (
     <div>
-      <h1>Employee List</h1>
-      <CardList />
+      <h1 style={{ textAlign: "center" }}>Employee List</h1>
+      <CardList employees={props.employees} />
     </div>
   );
 }
+
+export const getStaticProps = async () => {
+  let { data } = await axios(employees);
+  data = data.data;
+
+  return {
+    props: { employees: [...data] },
+  };
+};
