1914b4c9b4f8db9216764d208e949ec8e7f5eeb7
ialtafshaikh <altaf.shaikh@contentstack.com>
2021-01-06 22:24:44 +0530

    employee detail getStaticPaths implemented

--- pages/profile/[employeeId].js
+++ pages/profile/[employeeId].js
@@ -1,10 +1,13 @@
-import { getEmployeeById } from "../../constants/endpoints";
+import axios from "axios";
+
+import { getEmployeeById, employees } from "../../constants/endpoints";
 
 function EmployeeDetail(props) {
   return <div></div>;
 }
 
 export const getStaticProps = async (context) => {
+  console.log(context);
   let { data } = await axios(getEmployeeById + context.params.employeeId);
   data = data.data;
 
@@ -12,5 +15,19 @@ export const getStaticProps = async (context) => {
     props: { employee: [...data] },
   };
 };
+
+export async function getStaticPaths() {
+  let { data } = await axios(employees);
+  data = data.data;
+  let paths = data.map((employee) => ({
+    params: {
+      employeeId: `${employee.employeeId}`,
+    },
+  }));
+  return {
+    paths,
+    fallback: false, // See the "fallback" section below
+  };
+}
 
 export default EmployeeDetail;
