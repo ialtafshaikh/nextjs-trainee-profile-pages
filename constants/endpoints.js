7b7a4c5722a5bedee316e93040447d6db94b9694
ialtafshaikh <altaf.shaikh@contentstack.com>
2021-01-06 20:52:10 +0530

    add employee form implemnted

---
+++ constants/endpoints.js
@@ -0,0 +1,3 @@
+const BASE_URL = "https://trainee-profile-backend.herokuapp.com/";
+// const BASE_URL = "http://localhost:3001/;
+export const addEmployee = BASE_URL + "employees/addEmployee";
---
+++ pages/form.js
@@ -0,0 +1,75 @@
+import { Component } from "react";
+
+// endpoints
+import { addEmployee } from "../constants/endpoints";
+
+// styles
+import FormElements from "../components/FormElements";
+import FormElementStyles from "../styles/Form.module.css";
+
+//component to add employee form
+function AddEmployeeForm() {
+  //add new employee details from form
+  const submitForm = (event) => {
+    event.preventDefault();
+    let form = event.target;
+    let formObj = new FormData();
+    formObj.append("firstName", form.firstName.value);
+    formObj.append("lastName", form.lastName.value);
+    formObj.append("company", form.company.value);
+    formObj.append("email", form.email.value);
+    formObj.append("contact", form.contact.value);
+    formObj.append("imageUrl", form.imageUrl.files[0]);
+    formObj.append("skills", form.skills.value.split(","));
+    formObj.append("altDescription", form.altDescription.value);
+    formObj.append("hobbies", form.hobbies.value.split(","));
+    formObj.append("gitHub", form.gitHub.value);
+    formObj.append("linkedIn", form.linkedIn.value);
+    formObj.append("website", form.website.value);
+
+    fetch(addEmployee, {
+      method: "POST",
+      mode: "cors",
+      body: formObj,
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        form.reset();
+        alert("Form Submitted Successfully");
+        console.log(data);
+      })
+      .catch((err) => {
+        alert(`${form.firstName.value}, Please try again :)`);
+        alert(err);
+        console.log(err);
+      });
+  };
+  //markup for add employee form
+  return (
+    <div className={FormElementStyles.container}>
+      <form onSubmit={submitForm}>
+        <FormElements />
+        <div className={FormElementStyles.buttons}>
+          <button
+            type="submit"
+            value="submit"
+            className={FormElementStyles.button}
+          >
+            Submit
+          </button>
+          &nbsp;&nbsp;
+          <button
+            type="reset"
+            value="Reset"
+            className={FormElementStyles.button}
+          >
+            Reset
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
+export default AddEmployeeForm;
