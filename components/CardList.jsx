0426bcce4bf1910f6e6f2147daa55ef62a2c2aeb
ialtafshaikh <altaf.shaikh@contentstack.com>
2021-01-06 21:08:59 +0530

    cardlist component added

---
+++ components/CardList.jsx
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+
+// components
+import Card from "./Card";
+
+//styles
+import cardStyle from "../styles/Card.module.css";
+
+import { employees } from "../constants/endpoints";
+
+export default class CardList extends Component {
+  state = {
+    employees: [],
+  };
+
+  componentDidMount = () => {
+    fetch(employees)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        this.setState({ employees: data.data });
+      });
+  };
+
+  render() {
+    return (
+      <div className={cardStyle.cardContainer}>
+        {this.state.employees.map((employee) => {
+          return <Card employee={employee} key={employee.employeeId} />;
+        })}
+      </div>
+    );
+  }
+}
