8e48a825f75b512f8d826fc694092796891ef0ce
ialtafshaikh <altaf.shaikh@contentstack.com>
2021-01-06 22:04:53 +0530

    employee detail getStaticProps implemented

--- pages/profile/[employeeId].js
+++ pages/profile/[employeeId].js
@@ -1,5 +1,16 @@
+import { getEmployeeById } from "../../constants/endpoints";
+
 function EmployeeDetail(props) {
   return <div></div>;
 }
+
+export const getStaticProps = async (context) => {
+  let { data } = await axios(getEmployeeById + context.params.employeeId);
+  data = data.data;
+
+  return {
+    props: { employee: [...data] },
+  };
+};
 
 export default EmployeeDetail;
